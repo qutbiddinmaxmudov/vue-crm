2602553e2d8d426522541c99d5b8a768536933e1
Max <qutbiddinmaxmudov@mail.ru>
2020-11-01 00:59:49 +0500

    doing 5-lesson have some mistakes

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,30 +1,64 @@
 <template>
-<form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
-        <span class="card-title">Домашняя бухгалтерия</span>
-        <div class="input-field">
-            <input id="email" type="text" class="validate">
-            <label for="email">Email</label>
-            <small class="helper-text invalid">Email</small>
-        </div>
-        <div class="input-field">
-            <input id="password" type="password" class="validate">
-            <label for="password">Пароль</label>
-            <small class="helper-text invalid">Password</small>
-        </div>
+      <span class="card-title">Домашняя бухгалтерия</span>
+      <div class="input-field">
+        <input
+          id="email"
+          type="text"
+          v-model.trim="email"
+          :class="{
+            invalid:
+              ($v.email.$dirty && !$v.email.required) ||
+              ($v.email.$dirty && !$v.email.email)
+          }"
+        />
+        <label for="email">Email</label>
+        <small class="helper-text invalid">Email</small>
+      </div>
+      <div class="input-field">
+        <input id="password" type="password" class="validate" />
+        <label for="password">Пароль</label>
+        <small class="helper-text invalid">Password</small>
+      </div>
     </div>
     <div class="card-action">
-        <div>
-            <button class="btn waves-effect waves-light auth-submit" type="submit">
-                Войти
-                <i class="material-icons right">send</i>
-            </button>
-        </div>
+      <div>
+        <button class="btn waves-effect waves-light auth-submit" type="submit">
+          Войти
+          <i class="material-icons right">send</i>
+        </button>
+      </div>
 
-        <p class="center">
-            Нет аккаунта?
-            <a href="/">Зарегистрироваться</a>
-        </p>
+      <p class="center">
+        Нет аккаунта?
+        <router-link to="/register">Зарегистрироваться</router-link>
+      </p>
     </div>
-</form>
+  </form>
 </template>
+
+<script>
+import { email, required, minLength } from "vuelidate/lib/validators";
+export default {
+  name: "login",
+  data: () => ({
+    email: "",
+    password: ""
+  }),
+  validations: {
+    email: { email, required },
+    password: { required, minLength: minLength(8) }
+  },
+  methods: {
+    submitHandler() {
+      if (this.$v.invalid) {
+        this.$v.$touch();
+        return;
+      }
+      console.log(email);
+    //   this.$router.push("/");
+    }
+  }
+};
+</script>
